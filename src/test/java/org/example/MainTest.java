8ba2132ed6a137411cd86f2f36a4b2c5e3c5a353
hamidhosseini31397 <155641718+hamidhosseini31397@users.noreply.github.com>
2024-02-15 11:46:29 +0100

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,3 +1,4 @@
+
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,4 +9,15 @@ class MainTest {
     void testGreetUser() {
         assertEquals(0, Main.greetUser("John"));
     }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(8, Main.addNumbers(5, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testReverse() {
+        assertEquals("olleH", Main.reverseString("Hello"));
+    }
 }
+
