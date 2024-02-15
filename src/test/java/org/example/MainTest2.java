02006a07ffb954fe348c094b5d21f7cf2fa59d38
hamidhosseini31397 <155641718+hamidhosseini31397@users.noreply.github.com>
2024-02-15 11:42:06 +0100

    Update MainTest2.java

--- src/test/java/org/example/MainTest2.java
+++ src/test/java/org/example/MainTest2.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest2 {
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testReverse() {
         assertEquals("olleH", Main.reverseString("Hello"));
     }
